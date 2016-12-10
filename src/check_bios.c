639c2dd06bf012537c1b3e6b1d4ad88f9cdb8672
congweitao <congweitao@hotmail.com>
2016-12-10 19:47:40 +0800

    fix: memory leak

--- src/check_bios.c
+++ src/check_bios.c
@@ -13,30 +13,36 @@ int clushc_bios(
    char* item_content[NODE_NUM_MAX][2];
  
    /* make a decision and print the results */
-   printf("----\033[34m Cluster Health Checking System \033[0m----\n");
+   printf("----------------------------------\
+\033[34m Cluster Health Checking System \033[0m\
+----------------------------------\n");
+   printf(" HOSTNAME | HYPER THREADING | TOTAL MEM BARS | \
+LOAD AVG | CPU USAGE(%) | MEM SIZE(GB) | MEM USAGE(%) | STATUS  \n");
    for(i = 0; i< NODE_NUM_MAX; i++){
      if(strlen(node_list[i]) != 0){   
         /* get the checked-node item and the value */
         get_item_content(clushc_path,"THREAD_PER_CORE", item_content, node_list);
         if(atoi(item_content[i][1]) == 1) {
-               printf("[%-6s]  ->  Hyper-Threading--------[OFF]----[PASSWD]\n",item_content[i][0]);
+               printf("%-8s       OFF         ",item_content[i][0]);
             }
         else{
-               printf("[%-6s]  ->  Hyper-Threading--------[ON ]----\033[31m[NOT PASSWD]\033[0m\n",item_content[i][0]);
+               printf("%-8s        \033[31mON\033[0m         ",item_content[i][0]);
            }
         /* check total memory slots */
         get_item_content(clushc_path,"TOTAL_DIMMS", item_content, node_list);
-        printf("              Total Memory Slots-----[%-3s]----[PASSWD]\n",item_content[i][1]);
+        printf("         %-8s",item_content[i][1]);
 	/* check cpu load average */
         get_item_content(clushc_path,"LOAD_AVG", item_content, node_list);
-        printf("              Load Average-----------[%-3s]----[PASSWD]\n",item_content[i][1]);
+        printf("     %-8s",item_content[i][1]);
         /* check cpu used ratio (%) */
         get_item_content(clushc_path,"CPU_USED", item_content, node_list);
-        printf("              CPU USED Ratio(%)------[%-3s]----[PASSWD]\n",item_content[i][1]);
+        printf("    %-8s",item_content[i][1]);
+
         get_item_content(clushc_path,"MEMORY_SIZE", item_content, node_list);
-        printf("              Total Memory-----------[%-3s]----[PASSWD]\n",item_content[i][1]);
+        printf("    %-8s",item_content[i][1]);
+
         get_item_content(clushc_path,"MEM_USED_PERC", item_content, node_list);
-        printf("              Memory Usage-----------[%-3s]----[PASSWD]\n",item_content[i][1]);
+        printf("          %-8s\n",item_content[i][1]);
 
         sleep(1);
       }
--- src/check_firmware.c
+++ src/check_firmware.c
@@ -13,18 +13,20 @@ int clushc_firmware(
    int i=0;
    char* item_content[NODE_NUM_MAX][2];
 
-   printf("----------\033[34m Cluster Health Checking Firmware \033[0m----------\n");
+   printf("------------------\033[34m Cluster Health Checking Firmware \033[0m------------------\n");
+   printf(" HOSTNAME | IB FIRMWARE |BIOS FIRMWARE | BMC FIRMWARE | STATUS  \n");
+
    for(i = 0; i< NODE_NUM_MAX; i++){
       if(strlen(node_list[i]) != 0){
 	 /* infiniband firmware */
 	 get_item_content(clushc_path,"IB_FW", item_content, node_list);
-         printf("[%-6s]   -> The firmware of HCA card ---- %-16s\n",node_list[i],item_content[i][1]);
+         printf("%-8s    %-16s",node_list[i],item_content[i][1]);
 	 /* bios firmware */
 	 get_item_content(clushc_path,"BIOS_FW", item_content, node_list);
-         printf("           -> The firmware of BIOS is ----  %-16s\n",item_content[i][1]);
+         printf("  %-12s",item_content[i][1]);
 	 /* bios firmware */
 	 get_item_content(clushc_path,"BMC_FW", item_content, node_list);
-         printf("           -> The firmware of BMC is ---- %-16s\n",item_content[i][1]);
+         printf("  %-12s\n",item_content[i][1]);
          sleep(1);
       }
    }
